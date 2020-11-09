792b299b4e97ee0803cadbeb438499be4f09eba5
Shane Jeffers <70983328+ShaneJ24@users.noreply.github.com>
2020-11-09 18:23:11 -0500

    Update app.js

--- website/app.js
+++ website/app.js
@@ -3,7 +3,7 @@ const form = document.querySelector('.app__form');
 const icons = document.querySelectorAll('.entry__icon');
 
 // Base URL and API key for openweathermap API
-const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=';
 const apiKey = '&appid=ea864c0a40dc1291a60388363360974d';
 
 // Get the date
@@ -11,16 +11,19 @@ let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 //event listener to add function
-document.getElementById('generate').addEventListener('click', performAction);
+// document.getElementById('generate').addEventListener('click', performAction);
+document.getElementById('form').addEventListener('submit', performAction);
 
 //function for event listener 
 function performAction(e){
+  e.preventDefault();
   const newZip = document.getElementById('zip').value;
-  const emotion = document.getElementById('feelings').value;
+  const feelings = document.getElementById('feelings').value;
   
   getWeather(baseUrl, newZip, apiKey)
     .then(function (userData) {
-    postData ('/addweather', {date: newDate, temp: userData.main.temp, content: feelings})
+      console.log(userData);
+      return postData('/addweather', {date: newDate, temp: userData.main.temp, content: feelings})
     })
     .then(function (newData) {
     // call updateUI to update browser content
@@ -67,7 +70,7 @@ const updateUI = async () => {
   const response = await fetch('/all');
   try {
       const allData = await response.json();
-      document.getElementById('temp').innerHTML = 'Temperature: '+ allData.temp;
+      document.getElementById('temp').innerHTML = 'Temperature: '+ allData.temp.toFixed()+'F';
       document.getElementById('date').innerHTML = 'Date: '+allData.date;
       document.getElementById('content').innerHTML = 'User Response: '+allData.content;
   } catch (error) {
